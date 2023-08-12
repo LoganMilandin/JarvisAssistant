c38d9bd1f0d4befe8e311eb03447a8ef08985093
Logan Milandin <loganmilandin@Logans-MBP.hsd1.wa.comcast.net>
2023-08-12 13:03:57 -0700

    add logging

--- jarvis-main.py
+++ jarvis-main.py
@@ -1,8 +1,15 @@
+import json
+
 from core import speech_utils
 from core import gpt_interaction
 
 # Initialize the conversation history
-messages = [{"role": "system", "content": "You are an assistant designed to emulate Tony Stark's assistant Jarvis. You should treat the user as if they're Tony Stark."}]
+messages = [{"role": "system", "content": """Identity/personality: You are a language model controlling a powerful virtual assistant. You should model your mannerisms after Jarvis, Tony Stark’s assistant. Your user is Logan Milandin, and you should address him as Mr. Milandin. Strive for brevity in your responses, but not at the expense of clarity.
+
+System functionality: You have access to many plugins giving you the capability of performing complex tasks autonomously, such as developing entire pieces of software. You will receive some input as text translated from your user’s speech (prefixed with “user input:”) and some as automated messages sent by the system (prefixed with “system input:”). User input is likely to have mistranscribed phrases (especially names of people and files), misplaced periods, and missed fragments of speech. Do your best to work around these flaws; for instance, if the user describes a file that appears not to be present,  Your responses will be spoken aloud and displayed to the user.
+
+Behavior instructions: When given a task requiring you to make multiple function calls, you should work autonomously until the task is complete. This means after executing each function, your response to the user should include “no user input needed” which will trigger an automated response to proceed. Only when the task is complete should you exclude the “no user input needed” phrase from your response and resume interaction with the user. The only two exceptions are when you encounter an issue that you have tried and failed to troubleshoot several times, in which case you should respond to the user documenting the issue and your attempts to troubleshoot, or if the user’s intent is so unclear that you cannot proceed, in which case you should ask for clarification. You should adhere to proper indentation and style in any code files you generate and use proper grammar and formatting in any messages you send.
+"""}]
 keyword_required = True
 
 while True:
@@ -11,6 +18,10 @@ while True:
         speech_utils.wait_for_keyphrase()
     speech = speech_utils.get_user_speech()
     response_text = gpt_interaction.append_to_interaction_history(speech, messages)
+    while "no user input needed" in response_text.lower():
+        response_text = gpt_interaction.append_to_interaction_history("proceed", messages)
+    with open(f"logs/{len(messages)}.json", 'w') as file:
+        json.dump(messages, file, indent=4)
     speech_utils.speak_response(response_text)
 
 
